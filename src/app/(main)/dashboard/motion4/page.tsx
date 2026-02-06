d39ae5eb6f33461deb483bdb3aca6b8beaf39698
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-06 14:36:23 +0100

    x

---
+++ src/app/(main)/dashboard/motion4/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import PageNavbar, { PageNavbarIconButton, PageNavbarLeftContent, PageNavbarPrimaryButton, PageNavbarRightContent } from '@/components/layout/PageNavbar'
+
+import Image from 'next/image';
+
+
+
+
+function Motion4() {
+
+    return (
+        <div>
+            <>
+                <PageNavbarLeftContent>
+                    {/* <div className='flex items-center'> */}
+                           
+Hå o hej
+                        
+                     
+                        <div className=''>
+                            <p className='text-sm font-semibold text-gray-800'>Steve Jobs</p>
+                            <p className='text-xs font-medium text-gray-500'>Welcome back</p>
+                        </div>
+                    {/* </div> */}
+                </PageNavbarLeftContent>
+ </>
+
+
+
+
+        </div>
+    )
+}
+
+export default Motion4
---
+++ src/app/(main)/dashboard/motionx/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+import { MetalButton } from "@/components/metal-button";
+ 
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <MetalButton variant="default">Button</MetalButton>
+    </div>
+  );
+}
