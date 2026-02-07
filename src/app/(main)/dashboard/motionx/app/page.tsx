318665afcf5d61e8da4e9c3e0b2778fa719cea9f
Tomas <tomaslonnblad@gmail.com>
2026-02-07 15:31:01 +0100

    Update page.tsx

--- src/app/(main)/dashboard/motionx/app/page.tsx
+++ src/app/(main)/dashboard/motionx/app/page.tsx
@@ -4,8 +4,16 @@ import { MetalButton } from "@/components/metal-button";
  
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <MetalButton variant="default">Button</MetalButton>
-    </div>
-  );
-}
+      <main>
+     <div className="flex min-h-screen items-center justify-center">
+             <MetalButton variant="default">
+            <a
+            href="https://tomaslonnblad.github.io/"
+           
+          > 
+           3D </a></MetalButton>
+        </div> 
+    </main>
+
+  )}
+
